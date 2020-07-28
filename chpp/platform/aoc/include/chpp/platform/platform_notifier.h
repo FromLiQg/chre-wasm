29c2089273c1db0a31dcfd3176760ddb4bd264e3
Arthur Ishiguro <arthuri@google.com>
2020-07-28 20:41:18 +0000

    Revert "Adds skeleton for AoC chppNotifierTimedWait"
    This reverts commit 736e70841a2411ab32770ce17f8e443aeed9ce62.
    
    Test: Compiled
    Reason for revert: Since we now have a condition variable abstraction for CHPP, this timed wait is no longer needed.
    
    Change-Id: Ib281595ce3fddf6ff03446a41485b92482f64b43

--- chpp/platform/aoc/include/chpp/platform/platform_notifier.h
+++ chpp/platform/aoc/include/chpp/platform/platform_notifier.h
@@ -40,11 +40,9 @@ void chppPlatformNotifierInit(struct ChppNotifier *notifier);
 void chppPlatformNotifierDeinit(struct ChppNotifier *notifier);
 
 /**
- * Platform implementation of chppNotifier[Timed]Wait()
+ * Platform implementation of chppNotifierWait()
  */
 uint32_t chppPlatformNotifierWait(struct ChppNotifier *notifier);
-uint32_t chppPlatformNotifierTimedWait(struct ChppNotifier *notifier,
-                                       uint64_t timeoutNs);
 
 /**
  * Platform implementation of chppNotifierSignal()
@@ -61,11 +59,6 @@ static inline void chppNotifierDeinit(struct ChppNotifier *notifier) {
 
 static inline uint32_t chppNotifierWait(struct ChppNotifier *notifier) {
   return chppPlatformNotifierWait(notifier);
-}
-
-static inline uint32_t chppNotifierTimedWait(struct ChppNotifier *notifier,
-                                             uint64_t timeoutNs) {
-  return chppPlatformNotifierTimedWait(notifier, timeoutNs);
 }
 
 static inline void chppNotifierSignal(struct ChppNotifier *notifier,
